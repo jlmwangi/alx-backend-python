15d45acfaae81af6e6e740c51fcd2dfb4ecac949
mwangii <jlmwangi254@gmail.com>
2024-08-01 23:04:25 +0300

    delay random

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''the basics of async'''
+
+
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''takes an int arg, waits for a random delay btw 0
+    and max_delay and returns it'''
+    delay = random.uniform(0, max_delay)
+    return delay
