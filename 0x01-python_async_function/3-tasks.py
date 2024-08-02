d857e0e6e23ae98f97c063ce6549ee5e584fecf6
mwangii <jlmwangi254@gmail.com>
2024-08-02 05:55:10 +0300

    create relar function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     '''function that takes an int and returns an asyncio.task'''
     return asyncio.create_task(wait_random(max_delay))
