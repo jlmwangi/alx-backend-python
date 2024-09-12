efe8612587fb7994b1416325d33cc843fa495e84
mwangii <jlmwangi254@gmail.com>
2024-09-12 17:37:28 +0300

    error of the keys

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,15 @@ class TestAccessNestedMap(unittest.TestCase):
         '''test access nested map'''
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a"), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, KeyError):
+        '''use assert raises to test that a keyerror is raised'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == "__main__":
     unittest.main()
