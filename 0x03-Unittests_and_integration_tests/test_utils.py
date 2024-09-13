6bfdea6fd1b98c57787e1ef83d8bd959c1d73561
mwangii <jlmwangi254@gmail.com>
2024-09-13 08:28:54 +0300

    mock http calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, MagicMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +27,28 @@ class TestAccessNestedMap(unittest.TestCase):
         '''use assert raises to test that a keyerror is raised'''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''a class meant to test the get_json function'''
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')  # patching requests.get to return a mock response
+    def test_get_json(self, test_url, test_payload, mock_get_json):
+        '''method to test the get_json function'''
+        mock_response = MagicMock()  # a response object with a json method
+        mock_response.json.return_value = test_payload
+
+        '''set return value of requests.get to mock_response'''
+        mock_get_json.return_value = mock_response
+
+        '''call the get_json method: method being tested'''
+        result = get_json(test_url)
+
+        self.assertEqual(result, test_payload)
+        mock_get_json.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
