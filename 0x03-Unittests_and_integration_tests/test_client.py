6ead52d95ad175fa3a09c766c6c802ee71a1dfea
mwangii <jlmwangi254@gmail.com>
2024-09-14 11:42:47 +0300

    mock a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,8 +2,9 @@
 '''module to test client.GithubOrgClient'''
 
 
+import client
 from client import GithubOrgClient
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 import unittest
 from utils import get_json
@@ -12,8 +13,8 @@ from utils import get_json
 class TestGithubOrgClient(unittest.TestCase):
     '''class inheriting from unittest'''
     @parameterized.expand([
-        ('google', {"name": "google"}),
-        ('abc', {"name": "abc"})
+        ('google', {"login": "google"}),
+        ('abc', {"login": "abc"})
     ])
     @patch('utils.get_json')  # patch to make sure getjson is called once
     def test_org(self, name, result, mock_get_json):
@@ -23,3 +24,23 @@ class TestGithubOrgClient(unittest.TestCase):
         client_github = GithubOrgClient(name)
 
         self.assertEqual(client_github.org(), result)
+
+    @patch.object(client.GithubOrgClient, '_public_repos_url',
+                  new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_public_repos_property):
+        '''tests public_repos_url, a method in class githuborgclient'''
+        with patch('client.GithubOrgClient.org') as mock_github_org:
+            '''patch githuborg making it return a known payload'''
+            client_github = GithubOrgClient('google')
+            mock_github_org.return_value = {'public_repos_url':
+                                            'https://api.github.com'
+                                            '/orgs/google/repos'}
+            mock_public_repos_property.return_value = \
+                'https://api.github.com/orgs/google/repos'
+
+            self.assertEqual(client_github._public_repos_url,
+                             'https://api.github.com/orgs/google/repos')
+
+
+if __name__ == '__main__':
+    unittest.main()
