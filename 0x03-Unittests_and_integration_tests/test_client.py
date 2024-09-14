05ca31fd051c656bd14c45baafc5ff305130de78
mwangii <jlmwangi254@gmail.com>
2024-09-14 06:35:25 +0300

    try and fail

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''module to test client.GithubOrgClient'''
+
+
+from client import GithubOrgClient
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
+import unittest
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''class inheriting from unittest'''
+    @parameterized.expand([
+        ('google', {"name": "google"}),
+        ('abc', {"name": "abc"})
+    ])
+    @patch('client.get_json')  # patch to make sure getjson is called once
+    def test_org(self, name, result, mock_get_json):
+        '''tests that githuborgclient returns correct value'''
+        mock_get_json.return_value = result
+
+        client_github = GithubOrgClient(name)
+
+        self.assertEqual(client_github.org(), result)
